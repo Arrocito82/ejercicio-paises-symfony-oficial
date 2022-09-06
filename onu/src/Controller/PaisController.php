91a817211e6b6c00d262ff0bee8ab1152b1a9593
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2022-09-06 03:53:19 +0000

    controladores pais busqueda por codigo y todos los paises

--- onu/src/Controller/PaisController.php
+++ onu/src/Controller/PaisController.php
@@ -1,11 +1,11 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Pais;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Doctrine\Persistence\ManagerRegistry;
 class PaisController extends AbstractController
 {
     // la ruta en el browser es /paises/region/, con los parametros opcionales region 
@@ -31,23 +31,61 @@ class PaisController extends AbstractController
     }
 
     // notese que el nombre del parametro codigoPais es el mismo de la variable en la funcion detallePais, es decir $codigoPais
-    #[Route('/paises/{codigoPais}', name: 'detallePais', methods:['GET'])]
-    public function detallePais($codigoPais): JsonResponse
+    #[Route('/pais/{codigoPais}', name: 'detallePais', methods:['GET'])]
+    public function detallePais(ManagerRegistry $doctrine,$codigoPais): JsonResponse
     {
+        $pais = $doctrine->getRepository(Pais::class)->findOneBy(['codigo' => $codigoPais]);
+
+        if (!$pais) {
+            throw $this->createNotFoundException(
+                'No hay datos'
+            );
+        }
         
-        return $this->json([
-            'message' => 'Welcome to your new controller! '.$codigoPais,
-            'path' => 'src/Controller/PaisController.php',
-        ]);
+        $result[] = array(
+            'nombre' => $pais->getNombre(),
+            'capital' => $pais->getCapital(),
+            'poblacion' => $pais->getPoblacion(),
+            'moneda' => $pais->getMoneda(),
+            'idioma' => $pais->getIdioma(),
+        );
+
+        // return new JsonResponse(['paises'=>$result]);
+        return $this->json(['pais'=>$result]);
     }
 
-    #[Route('/paises/', name: 'listaPaises', methods:['GET'])]
-    public function lista(): JsonResponse
-    {
+    // #[Route('/paises/', name: 'listaPaises', methods:['GET'])]
+    // public function lista(): JsonResponse
+    // {
         
-        return $this->json([
-            'message' => 'Welcome to your new controller! ',
-            'path' => 'src/Controller/PaisController.php',
-        ]);
+    //     return $this->json([
+    //         'message' => 'Welcome to your new controller! ',
+    //         'path' => 'src/Controller/PaisController.php',
+    //     ]);
+    // }
+    #[Route('/paises/', name: 'listaPaises')]
+    public function listaPaises(ManagerRegistry $doctrine): JsonResponse
+    {
+        $paises = $doctrine->getRepository(Pais::class)->findAll();
+
+        if (!$paises) {
+            throw $this->createNotFoundException(
+                'No hay datos'
+            );
+        }
+        
+        $result = array();
+
+        foreach($paises as $pais) {
+            $result[] = array(
+                'nombre' => $pais->getNombre(),
+                'capital' => $pais->getCapital(),
+                'poblacion' => $pais->getPoblacion(),
+                'moneda' => $pais->getMoneda(),
+                'idioma' => $pais->getIdioma(),
+            );
+        }
+
+        return new JsonResponse(['paises'=>$result]);
     }
 }
